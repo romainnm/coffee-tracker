95a26d2ac7388bd4c1f4acd6a1263e761d7da3e9
Romain <insc.romain@gmail.com>
2024-07-14 22:22:03 -0400

    chore: update coffee controller for postman setup

--- controllers/coffee.controller.ts
+++ controllers/coffee.controller.ts
@@ -6,19 +6,19 @@ const getAllCoffeeBags = async (req: Request, res: Response): Promise<void> => {
 };
 
 const createCoffeeBag = async (req: Request, res: Response): Promise<void> => {
-    res.status(200).json(MockCoffeeBags);
+    res.status(200).send("Coffee created");
 };
 
 const getCoffeeBag = async (req: Request, res: Response): Promise<void> => {
-    res.status(200).json(MockCoffeeBags);
+    res.status(200).json(MockCoffeeBags[0]);
 };
 
 const updateCoffeeBag = async (req: Request, res: Response): Promise<void> => {
-    res.status(200).json(MockCoffeeBags);
+    res.status(200).send("Coffee info updated");
 };
 
 const deleteCoffeeBag = async (req: Request, res: Response): Promise<void> => {
-    res.status(200).json(MockCoffeeBags);
+    res.status(200).send("Coffee deleted");
 };
 
 export {
