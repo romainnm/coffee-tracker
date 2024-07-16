843cfc77d63158576448f5046120456643211038
Romain <insc.romain@gmail.com>
2024-07-16 14:24:19 -0400

    feat: add coffeebag schema

---
+++ models/coffee.model.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import { ICoffeeBag } from "../interfaces";
+
+const CoffeeBagSchema = new mongoose.Schema<ICoffeeBag>({
+    name: {
+        type: String,
+        required: [true, "name must be provided"],
+        trim: true,
+        maxlength: [30, "name is limited to 30 chars"],
+    },
+    brand: {
+        type: String,
+        required: [true, "brand must be provided"],
+        trim: true,
+        maxlength: [30, "name is limited to 30 chars"],
+    },
+    status: {
+        type: String,
+        enum: {
+            values: ["new", "in progress", "finished"],
+        },
+        default: "new",
+    },
+    archived: {
+        type: Boolean,
+        default: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+});
+
+const CoffeeBagModel = mongoose.model("CoffeeBag", CoffeeBagSchema);
+
+export { CoffeeBagModel };
